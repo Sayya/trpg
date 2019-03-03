f7e965c05d5b4e1b60b7ecd56fd1dc3b54107eca
Saiah <sheepwords@gmail.com>
2019-03-04 03:29:34 +0900

    Update trpg.py

--- trpg.py
+++ trpg.py
@@ -6,10 +6,13 @@ class Dice:
     def __init__(self, times, maxim):
         self.times = times
         self.maxim = maxim
-        self.dice = lambda: sum(random.randint(1, self.maxim) for i in range(self.times))
-        self.wall = lambda: self.times * self.maxim / 2
 
-class Param():
+        # 戻値：数値
+        self.dice = lambda: sum(random.randint(1, self.maxim) for i in range(self.times))
+        self.ceil = lambda: self.times * self.maxim
+        self.wall = lambda: self.ceil() / 2
+
+class Param:
     """ Thingの属性（パラメータ）になる """
     master = dict()
 
@@ -19,14 +22,36 @@ class Param():
         Param.master[self.name] = self
 
         # pointA: 重み(0 <= n <= 1)とパラメータ(params[i])の数値を掛けたものの和
-        self.pointA = lambda params: sum(Param.master[i].point(params) * self.weight[i] for i in self.weight.keys())
-        # pointB: 当クラスのパラメータ(params[self.name])の数値に残りの重みパーセンテージ(1 - sum(n))をかけた数値
-        self.pointB = lambda params: params[self.name] * (1 - sum(self.weight[i] for i in self.weight.keys()))
-        # pointAとpointBの和、pointAで遡って各子Paramの重み計算が動的にできる
+        self.pointA = lambda params: sum(Param.master[i].point(params) * self.weight[i] for i in self.weight.keys() if i != self.name)
+        # pointB: 当クラスのパラメータ(params[self.name])の数値に重みを掛けた数値
+        self.pointB = lambda params: params[self.name] * self.weight[self.name]
+
+        # pointAとpointBの和、pointAで遡って子Paramの重み計算が動的にできる
+        # 引数：Thingの固有パラメータリスト
+        # 戻値：数値
         self.point = lambda params: self.pointA(params) + self.pointB(params)
 
+        # 戻値：数値
         self.dice = dice.dice
+        self.ceil = dice.ceil
         self.wall = dice.wall
+
+class Multi:
+    """ Thingの属性（パラメータ）になる② """
+    master = dict()
+
+    def __init__(self, name, param1, param2, param3):
+        self.name = name
+        self.main = param1
+        self.subj_modif = param2
+        self.obj_modif = param3
+        Multi.master[self.name] = self
+
+    # ２つのThingが競う
+    def compare(self, subj, obj):
+        offence = subj.dice(self.main) + subj.point(self.subj_modif)
+        defence = obj.wall(self.main) + obj.point(self.obj_modif)
+        return offence - defence
 
 class Thing:
     """ 対象となるもの、オブジェクト """
@@ -35,29 +60,52 @@ class Thing:
         self.params = dic
 
         # パーセンテージ（HPなど）
-        self.params_tmp = dict()
+        self.p_ratio = dict()
         for i in self.params:
-            self.params_tmp[i] = 1
+            self.p_ratio[i] = 1
 
+        # 引数：対象のパラメータ
         self.point = lambda param: param.point(self.params)
+        self.dice = lambda param: param.point(self.params) * param.dice()
+        self.ceil = lambda param: param.point(self.params) * param.ceil()
+        self.wall = lambda param: param.point(self.params) * param.wall()
 
-        self.dice = lambda param: self.point(param) * param.dice()
-        self.wall = lambda param: self.point(param) * param.wall()
+        self.hp = lambda param: self.p_ratio[param.name] * self.ceil(param)
+
+    def decreace(self, param, n):
+        self.p_ratio[param.name] = (self.ceil(param) * self.p_ratio[param.name] - n) / self.ceil(param)
 
 class Progress:
     """
     1. 比較し結果を得る
     2. 結果をもとにパラメータを変動させる
+    3. パラメータの変動の結果、終了条件に適合すれば結果処理をする
     """
+    
+    def __init__(self, multi):
+        self.multi = multi
+
+    def decreace(self, subj, obj):
+        n = self.multi.compare(subj, obj)
+        print(self.multi.name, 'damage:',  int(n))
+        if n > 0:
+            obj.decreace(self.multi.main, n)
+        return obj.p_ratio[self.multi.main.name]
+
+    def next(self, subj, obj):
+        r = self.decreace(subj, obj)
+        print(obj.name, 'HP:', int(obj.hp(self.multi.main)))
+        print(obj.name, 'ratio:', int(r * 100), '%')
+        if r <= 0:
+            return False
+        else:
+            return True
+
+class Event:
+    """ イベントの設計 """
     def __init__(self):
         pass
 
 class Game:
-    """ 引数にParamを一つ渡し、それについて２つのThingが競う """
-    def __init__(self, param):
-        self.param = param
-        
-        self.point = lambda subj: subj.dice(self.param)
-        self.wall = lambda obj: obj.wall(self.param)
-
-        self.compare = lambda subj, obj: subj.dice(self.param) - obj.center(self.param)
+    def __init__(self):
+        pass
