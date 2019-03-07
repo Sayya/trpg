84101da6d0ead4dd952ea542f8c896dd7ce49894
Saiah <sheepwords@gmail.com>
2019-03-08 08:56:09 +0900

    Update trpg.py

--- trpg.py
+++ trpg.py
@@ -3,14 +3,16 @@ import random
 
 class Dice:
     """ 2d6形式を表現できる乱数クラス """
-    def __init__(self, times, maxim):
+    def __init__(self, times, maxim, offset=0):
         self.times = times
         self.maxim = maxim
+        self.offst = offset
 
         # 戻値：数値
-        self.dice = lambda: sum(random.randint(1, self.maxim) for i in range(self.times))
-        self.ceil = lambda: self.times * self.maxim
-        self.wall = lambda: self.ceil() / 2
+        self.dice = lambda: sum(random.randint(1, self.maxim) for i in range(self.times)) + self.offst
+        self.ceil = lambda: self.times * self.maxim + self.offst
+        self.wall = lambda: self.times * (self.maxim + 1) / 2 + self.offst
+        self.flor = lambda: self.times * self.maxim + self.offst
 
 class Param:
     """ Thingの属性（パラメータ）になる """
@@ -24,7 +26,7 @@ class Param:
         # pointA: 重み(0 <= n <= 1)とパラメータ(params[i])の数値を掛けたものの和
         self.pointA = lambda params: sum(Param.master[i].point(params) * self.weight[i] for i in self.weight.keys() if i != self.name)
         # pointB: 当クラスのパラメータ(params[self.name])の数値に重みを掛けた数値
-        self.pointB = lambda params: params[self.name] * self.weight[self.name]
+        self.pointB = lambda params: params[self.name] * self.weight[self.name] if self.name in params else 0
 
         # pointAとpointBの和、pointAで遡って子Paramの重み計算が動的にできる
         # 引数：Thingの固有パラメータリスト
@@ -38,9 +40,17 @@ class Param:
 
 class Thing:
     """ 対象となるもの、オブジェクト """
-    def __init__(self, name, dic):
+    master = dict()
+    pid = 0
+
+    def __init__(self, name, dic1, dic2):
         self.name = name
-        self.params = dic
+        self.params = dic1
+        self.propts = dic2
+        self.pid = Thing.pid
+
+        Thing.master[Thing.pid] = self
+        Thing.pid += 1
 
         # パーセンテージ（HPなど）
         self.p_ratio = dict()
@@ -55,26 +65,30 @@ class Thing:
     def change(self, param, n):
         self.p_ratio[param.name] = (self.bar(param) + n) / self.siz(param)
 
+class Roll:
+    """ 権限の単位、TRPGにおけるPL、PCのPLにあたる """
+    def __init__(self, name, dic):
+        self.name = name
+        self.propts = dic
+
+
 class Multi:
     """ Thingの属性（パラメータ）になる② """
     master = dict()
 
-    def __init__(self, name, param0, param1, param2, things1, things2):
+    def __init__(self, name, param0, param1, param2):
         self.name = name
         self.target = param0
 
         self.sbj_param = param1
         self.obj_param = param2
 
-        self.sbj_things = things1
-        self.obj_things = things2
-
         Multi.master[self.name] = self
 
     # ２つのThingが競う
     def compare(self, sbj, obj):
-        sbj_things_sum = sum(self.sbj_things[i].point(self.target) for i in self.sbj_things.keys()) + 1
-        obj_things_sum = sum(self.obj_things[i].point(self.target) for i in self.obj_things.keys()) + 1
+        sbj_things_sum = sum(sbj.propts[i].point(self.target) for i in sbj.propts.keys() if self.target.name in sbj.propts[i].params) + 1
+        obj_things_sum = sum(obj.propts[i].point(self.target) for i in obj.propts.keys() if self.target.name in sbj.propts[i].params) + 1
         
         offence = sbj.point(self.target) * sbj_things_sum * self.target.dice() + sbj.point(self.sbj_param)
         defence = obj.point(self.target) * obj_things_sum * self.target.dice() + obj.point(self.obj_param)
@@ -108,13 +122,24 @@ class Progress:
         else:
             return True
 
-class Cost:
-    """ コストの設計 """
+class Gate:
+    """
+    Thingのパラメータへ干渉するための関門 
+    1. コスト管理
+    """
     def __init__(self):
         pass
+    
+    def add(self, thing, param, n):
+        thing.params[param.name] += n
 
 class Select:
     """ 選択の設計 """
+    def __init__(self):
+        pass
+
+class Place:
+    """ 場所の設計 """
     def __init__(self):
         pass
 
