807e0699da8db25b54f73a9f2dc1e90025d6b71a
Saiah <sheepwords@gmail.com>
2019-04-17 12:48:27 +0900

    Update iotemp.py

--- iotemp.py
+++ iotemp.py
@@ -1,9 +1,11 @@
 from inspect import signature
 
-class ClassTemp:
+class Template:
     def __init__(self, classt):
         self.sig = signature(classt.__init__).parameters
 
         for i in list(self.sig)[1:]:
-            print(self.sig[i])
+            print('<name>={0} : <default>={1} : <class>={3}'.format(self.sig[i].name, self.sig[i].default, self.sig[i].default.__class__))
+            chkstr = input()
+            arglis[i] = chkstr
 
